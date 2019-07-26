33f520d39459cae4395bbaa5ea8f8634a51a27ca
Ricardo Pinnock <ricardopinnock00@gmail.com>
2019-07-25 17:41:24 -0700

    list page draft 1

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+// const homepagebutton = document.querySelector("#homepagebutton");
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Bathroom Mapper' });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Bathroom Mapper" });
 });
 
+// homepagebutton.addEventListener("click", event => {
+//   window.location.href = "www.google.com";
+// });
+
+// $("#homepagebutton").on("click",  () => {
+//   $("homepagebutton").transition('pulse')
+
+// }
+
+// $("#greenbutton").on("click", () => {
+//   // pulse the button
+//   $("#greenbutton").transition('pulse')
+//   console.log($("#greenbuttonsound"))
+//   function stopAudio() {
+//       let sounds = document.getElementsByTagName('audio');
+//       for(i=0; i<sounds.length; i++) sounds[i].pause();
+//      };
+//   document.querySelector("#greenbuttonsound").play();
+// });
+
+// function toLists() {
+//   window.location.href = '';
+// }
+
+// toLists()
 module.exports = router;
