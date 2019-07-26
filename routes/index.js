a34b6bbba26a56b8c241ef5f5f529f1761c3b44f
Ricardo Pinnock <ricardopinnock00@gmail.com>
2019-07-25 17:53:12 -0700

    draft 2

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  res.render("index", { title: "Bathroom Mapper" });
+  res.render("index", { title: "Potty Picker" });
 });
 
 // homepagebutton.addEventListener("click", event => {
